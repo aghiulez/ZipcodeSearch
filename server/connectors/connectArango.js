f88694df69316e36aac26a6558c05c00be2ea2f9
Ara <aghiulezian@gmail.com>
2019-12-18 21:11:49 +0400

    dockerized api + db

--- server/connectors/connectArango.js
+++ server/connectors/connectArango.js
@@ -4,34 +4,38 @@
 const {InputModel} = require("../models/input");
 const orango = require("orango")
 
+require('dotenv').config();
 
 const { EVENTS } = orango.consts;
+
+
 
 async function connectArangoDB() {  // async functions return promises!
   try {
 
     const orangoInstance = orango.get("dev");
-
     // Bind Model to orango instance
     InputModel(orangoInstance);
 
     
 
     orangoInstance.events.once(EVENTS.CONNECTED, conn => {
-      console.log('🥑  Connected to ArangoDB:', conn.url + '/_db/' + conn.name);
+      console.log('🥑  Connected to ArangoDB:', conn.url + '_db/' + conn.name);
     });
     orangoInstance.events.once(EVENTS.READY, () => {
       console.log('🍊  Models loaded, Orango is all good to go!');
     });
     await orangoInstance.connect({
-       url: 'http://localhost:8530',
-       username: 'root', 
-       password: '' 
+      url: process.env.DB_HOST,
+      username: 'root', 
+      password: '' 
     });
+
     return orangoInstance;
     
   } 
   catch (dbInitError){
+    console.log(process.env.DB_HOST)
     console.log(dbInitError);
   }
 }
